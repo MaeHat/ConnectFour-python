c2d0f177a7a4b65a0760945cfc3a702eecfd9f5f
Israel <MaeHat@users.noreply.github.com>
2015-05-26 16:51:31 +0100

    Update connect4.py

--- connect4.py
+++ connect4.py
@@ -2,7 +2,9 @@
 __author__ = 'Israel Estévez Hatchuell'
 
 import games
+
 import heu
+
 
 def next_state(state, game):
     while True:
@@ -14,64 +16,57 @@ def next_state(state, game):
     return game.make_move(column, state)
 
 
+def difficulty():
+    while True:
+        try:
+            dif = int(raw_input("Choose difficulty you are willing to tolerate (Easy=1, Medium=2, Hard=3): "))
+            if dif == 1:
+                return 0
+            elif dif == 2:
+                return 2
+            elif dif == 3:
+                return 4
+        except ValueError:
+            print "Write 1 to easy, 2 to medium or 3 to hard level!"
+
+
+def players():
+    while True:
+        answer = raw_input("Would you like the computer as your adversary?(yes/no): ")
+        if answer == 'yes':
+            return True
+        elif answer == 'no':
+            print "Ok, you will play with your friend. Enjoy!"
+            return False
+        print "Just write yes or no!"
+
 game = games.ConnectFour()
 state = game.initial
 player = game.to_move(state)
-
 welcome = "New Game of {0} and begin the player {1}"
 print welcome.format(game, player).center(80, "*")
+adversary_computer = players()
+if adversary_computer:
+    depth = difficulty()
+
 
 while True:
     print "It's up to: ", game.to_move(state)
     print "Legal moves: ", game.legal_moves(state)
     if player == 'X':
-        move = games.alphabeta_search(state, game, d=2, eval_fn=heu.heuristic1)
-        state = game.make_move(move, state)
-        player = game.to_move(state)
+        if adversary_computer:
+            move = games.alphabeta_search(state, game, d=depth, eval_fn=heu.heuristic)
+            state = game.make_move(move, state)
+        else:
+            state = next_state(state, game)
     else:
         state = next_state(state, game)
-        player = game.to_move(state)
     game.display(state)
     print "-------------------"
     if game.terminal_test(state):
-        winner = game.to_move(state)
-        if winner == 'X':
-            winner = 'O'
+        if state.utility != 0:
+            print "The winner is: ", player
         else:
-            winner = 'X'
-        print "Final de la partida, el ganador es: ", winner
+            print "Draw!"
         break
-
-
-# x = 0
-# o = 0
-#
-# for i in range(10):
-#     game = games.ConnectFour()
-#     state = game.initial
-#     player = game.to_move(state)
-#     while True:
-#         if player == 'X':
-#             move = games.alphabeta_search(state, game, eval_fn=heu.heuristic1)
-#             state = game.make_move(move, state)
-#             player = game.to_move(state)
-#         else:
-#             move = games.alphabeta_search(state, game, eval_fn=heu.heuristic2)
-#             state = game.make_move(move, state)
-#             # state = next_state(state, game)
-#             player = game.to_move(state)
-#         game.display(state)
-#         print "-------------------"
-#         if game.terminal_test(state):
-#             winner = game.to_move(state)
-#             if winner == 'X':
-#                 winner = 'O'
-#                 o += 1
-#             else:
-#                 winner = 'X'
-#                 x += 1
-#             print "Final de la partida, el ganador es: ", winner
-#             break
-# print "Total de veces ganadas X: ", x
-# print "Total de veces ganadas O: ", o
-
+    player = game.to_move(state)
